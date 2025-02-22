1f90a8641063829038fcdaea1b4c89403a5a9934
imsofuckingdone11212 <bcook19292+39@gmail.com>
2025-02-22 13:28:26 -0500

    Create run-dev.sh

---
+++ .github/workflows/run-dev.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+rm /home/arman/.config/Electron/SingletonLock
+rm -r /home/arman/.config/Electron/Cache
+
+rm -r /home/arman/ledger-live/apps/ledger-live-desktop/dist
+rm -r /home/arman/ledger-live/apps/ledger-live-desktop/.webpack
+
+pnpm store prune
+pnpm rebuild
+pnpm run dev:lld
